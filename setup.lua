3f213c6f78e5d51e300725fcfcca01c53160ca05
akatsuki <139763685+akatsuki033896@users.noreply.github.com>
2026-01-10 14:33:15 +0800

    Update setup.lua
    upgrade lsp.nvim and use new syntax to setup lsp

--- setup.lua
+++ setup.lua
@@ -1,15 +1,23 @@
 require("plugins")
-local lspconfig = require("lspconfig")
+
+-- local lspconfig = require("lspconfig")
+-- local lspconfig = vim.lsp.config
+
 require("mason-lspconfig").setup({
     ensured_installed = {},
     automatic_installation = true,
 })
 
-lspconfig.lua_ls.setup {}
-lspconfig.clangd.setup {}
---lspconfig.rust_analyzer.setup {}
---lspconfig.gopls.setup {}
---lspconfig.pyright.setup {}
---lspconfig.yamlls.setup {}
---lspconfig.bashls.setup {}
-lspconfig.cmake.setup {}
+vim.lsp.enable('bashls')
+vim.lsp.enable('lua_ls')
+vim.lsp.enable('pyright')
+vim.lsp.enable('clangd')
+
+-- lspconfig.lua_ls.setup {}
+-- lspconfig.clangd.setup {}
+-- lspconfig.rust_analyzer.setup {}
+-- lspconfig.gopls.setup {}
+-- lspconfig.pyright.setup {}
+-- lspconfig.yamlls.setup {}
+-- lspconfig.bashls.setup {}
+-- lspconfig.cmake.setup {}
